29f357a89e90f8e61fecb78d8ecf0970fcdb6913
Akarshak670 <120705719+Akarshak670@users.noreply.github.com>
2023-07-24 07:24:18 +0000

    ascii code

---
+++ akarshak04.java
@@ -0,0 +1,10 @@
+//WAP to encrypt a grade by adding 8 to it .Decrypt it to show the correct grade
+public class akarshak04 {
+    public static void main(String[] args) {
+        char grade = 'B';
+        grade = (char) (grade + 8);
+        System.out.println(grade);
+        grade = (char) (grade - 8);
+        System.out.println(grade);
+    }
+}
